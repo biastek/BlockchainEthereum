0cdbb658ac0301409820878f0f3a88ce8aaece15
humichau\minhchau <minhchau2611@gmail.com>
2019-06-14 08:30:49 +0700

    Blockchain ifarm version 1.0

---
+++ scripts/prereqs-app.sh
@@ -0,0 +1,109 @@
+#!/bin/bash
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Usage:
+#
+# ./prereqs-app.sh
+#
+# User must then logout and login upon completion of script
+#
+
+# Exit on any failure
+set -e
+
+# Array of supported versions
+declare -a versions=('trusty' 'xenial' 'yakkety');
+
+# check the version and extract codename of ubuntu if release codename not provided by user
+if [ -z "$1" ]; then
+    source /etc/lsb-release || \
+        (echo "Error: Release information not found, run script passing Ubuntu version codename as a parameter"; exit 1)
+    CODENAME=${DISTRIB_CODENAME}
+else
+    CODENAME=${1}
+fi
+
+# check version is supported
+if echo ${versions[@]} | grep -q -w ${CODENAME}; then
+    echo "Installing Hyperledger Composer prereqs for Ubuntu ${CODENAME}"
+else
+    echo "Error: Ubuntu ${CODENAME} is not supported"
+    exit 1
+fi
+
+# Update package lists
+echo "# Updating package lists"
+sudo apt-add-repository -y ppa:git-core/ppa
+sudo apt-get update
+
+# Install Curl
+echo "# Installing Curl"
+sudo apt-get install -y curl
+
+# Install Git
+echo "# Installing Git"
+sudo apt-get install -y git
+
+# Install nvm dependencies
+echo "# Installing nvm dependencies"
+sudo apt-get -y install build-essential libssl-dev
+
+# Execute nvm installation script
+echo "# Executing nvm installation script"
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash
+
+# Set up nvm environment without restarting the shell
+export NVM_DIR="${HOME}/.nvm"
+[ -s "${NVM_DIR}/nvm.sh" ] && . "${NVM_DIR}/nvm.sh"
+[ -s "${NVM_DIR}/bash_completion" ] && . "${NVM_DIR}/bash_completion"
+
+# Install node
+echo "# Installing nodeJS"
+nvm install --lts
+
+# Configure nvm to use version 6.9.5
+nvm use --lts
+nvm alias default 'lts/*'
+
+# Install the latest version of npm
+echo "# Installing npm"
+npm install npm@latest -g
+
+# Install python v2 if required
+set +e
+COUNT="$(python -V 2>&1 | grep -c 2.)"
+if [ ${COUNT} -ne 1 ]
+then
+   sudo apt-get install -y python-minimal
+fi
+
+# Print installation details for user
+echo ''
+echo 'Installation completed, versions installed are:'
+echo ''
+echo -n 'Curl:           '
+curl --version
+echo -n 'Node:           '
+node --version
+echo -n 'npm:            '
+npm --version
+echo -n 'Git:         '
+git --version
+echo -n 'Python:         '
+python -V
+
+# Print reminder of need to logout in order for these changes to take effect!
+echo ''
+echo "Please logout then login before continuing."
---
+++ scripts/prereqs-ubuntu.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Usage:
+#
+# ./prereqs-ubuntu.sh
+#
+# User must then logout and login upon completion of script
+#
+
+# Exit on any failure
+set -e
+
+# Array of supported versions
+declare -a versions=('trusty' 'xenial' 'yakkety', 'bionic');
+
+# check the version and extract codename of ubuntu if release codename not provided by user
+if [ -z "$1" ]; then
+    source /etc/lsb-release || \
+        (echo "Error: Release information not found, run script passing Ubuntu version codename as a parameter"; exit 1)
+    CODENAME=${DISTRIB_CODENAME}
+else
+    CODENAME=${1}
+fi
+
+# check version is supported
+if echo ${versions[@]} | grep -q -w ${CODENAME}; then
+    echo "Installing Hyperledger Composer prereqs for Ubuntu ${CODENAME}"
+else
+    echo "Error: Ubuntu ${CODENAME} is not supported"
+    exit 1
+fi
+
+# Update package lists
+echo "# Updating package lists"
+sudo apt-add-repository -y ppa:git-core/ppa
+sudo apt-get update
+
+# Install build-essential
+echo "# Installing build-essential"
+sudo apt-get install build-essential
+
+# Upgrade package lists
+echo "# Upgrade packages"
+sudo apt-get upgrade
+
+
+
+# Print reminder of need to logout in order for these changes to take effect!
+echo ''
+echo "prereqs-ubuntu is finished."
